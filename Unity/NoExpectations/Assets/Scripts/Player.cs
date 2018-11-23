d5f1b4ed7473b6f00b7b71de21cc04450d748e30
Chris Deliz <chris.deliz@gmail.com>
2018-11-23 17:13:30 -0500

    Added player object as test for commit validity.

---
+++ Unity/NoExpectations/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private struct statsSet
+    {
+        int stat1;
+        int stat2;
+        int stat3;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
